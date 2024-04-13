205a0b6be856e7db15745ddf33ade70bd847cfd5
Akhil-ps-dev <psakhil211@gmail.com>
2024-04-13 14:14:44 +0530

    updated design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,8 +17,8 @@ class HomeScreen extends StatelessWidget {
         title: Row(
           children: [
             Image.asset(SystemImages.logo, height: 30, width: 30),
-            SizedBox(width: 5),
-            Text(
+            const SizedBox(width: 5),
+            const Text(
               'Interview360°',
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
@@ -26,78 +26,138 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         actions: [
-          TextButton(
-              onPressed: () {},
-              child: Text('Home', style: TextStyle(color: Colors.white))),
-          TextButton(
-              onPressed: () {},
-              child: Text('Interview Prep',
-                  style: TextStyle(color: Colors.white))),
-          TextButton(
-              onPressed: () {},
-              child: Text('Resume Reviewer',
-                  style: TextStyle(color: Colors.white))),
-          TextButton(
-              onPressed: () {},
-              child: Text('About Us', style: TextStyle(color: Colors.white))),
-          TextButton(
-              onPressed: () {},
-              child: Text('Contact Us', style: TextStyle(color: Colors.white))),
+          _buildTextButton('Home'),
+          _buildTextButton('Interview Prep'),
+          _buildTextButton('Resume Reviewer'),
+          _buildTextButton('About Us'),
+          _buildTextButton('Contact Us'),
         ],
       ),
-      body: Column(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    child: Text('Question:'),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    child: Text('Sample Interview Question?'),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  padding: EdgeInsets.all(8),
-                ),
-              ),
-            ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildQuestionSection(),
+              ],
+            ),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Text('Sample Interview Question?'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: TextField(
-                    decoration: InputDecoration(hintText: 'Your answer here'),
-                  ),
-                ),
-              ),
-            ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildInfoContainer(),
+                _buildTipsSection(),
+              ],
+            ),
           ),
         ],
       ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.all(8.0),
-        child:
-            Text('Tips using the STAR method: Situation, Task, Action, Result'),
+    );
+  }
+
+  Widget _buildTextButton(String title) {
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w100,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildQuestionSection() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Question:',
+              style: TextStyle(
+                color: SystemColors.headerColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              )),
+          const Text(' Why do you want to work here?',
+              style: TextStyle(
+                color: SystemColors.headerColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              )),
+          const SizedBox(height: 20),
+          const Text('Your answer:',
+              style: TextStyle(
+                color: SystemColors.headerColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              )),
+          const SizedBox(height: 15),
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              color: SystemColors.answerBoxClr,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              maxLines: null,
+              minLines: null,
+              expands: true,
+              textAlignVertical: TextAlignVertical.top,
+              textAlign: TextAlign.start,
+              decoration: InputDecoration(
+                hintText: 'Type your answer here',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: SystemColors.answerBoxClr,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoContainer() {
+    return Container(
+      width: 500,
+      height: 100,
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: const EdgeInsets.all(8),
+    );
+  }
+
+  Widget _buildTipsSection() {
+    return const Padding(
+      padding: EdgeInsets.all(8),
+      child: Text(
+        'Tips using the STAR method: Situation, Task, Action, Result',
+        style: TextStyle(
+          color: SystemColors.headerColor,
+          fontWeight: FontWeight.w500,
+          fontSize: 17,
+        ),
       ),
     );
   }
